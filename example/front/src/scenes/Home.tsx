27914feaeb8e98d73bf72c99b33abc9488a97883
David Hérault <dherault@gmail.com>
2022-05-20 02:51:22 +0300

    Create players

--- example/front/src/scenes/Home.tsx
+++ example/front/src/scenes/Home.tsx
@@ -1,15 +1,48 @@
-import { Div, H1, Section } from 'honorable'
+import { useState } from 'react'
+import { Button, Div, Flex, H1, Section } from 'honorable'
+
+import Player from '../components/Player'
 
 function Home() {
+  const [nPlayers, setNPlayers] = useState(0)
+
+  function handleCreate100Players() {
+    setNPlayers(x => x + 100)
+  }
+
+  const players = []
+
+  for (let i = 0; i < nPlayers; i++) {
+    players.push(<Player key={i} />)
+  }
+
+  console.log('players', players)
+
   return (
     <Section
       container
       pt={2}
       pb={4}
+      height="100vh"
+      display="flex"
+      flexDirection="column"
     >
-      <H1>Home</H1>
-      <Div mt={2}>
-        App
+      <Flex>
+        <H1>Localities</H1>
+        <Button
+          ml={2}
+          onClick={handleCreate100Players}
+        >
+          Create 100 players
+        </Button>
+      </Flex>
+      <Div
+        mt={2}
+        flexGrow={1}
+        border="1px solid border"
+        position="relative"
+      >
+        {players}
       </Div>
     </Section>
   )
